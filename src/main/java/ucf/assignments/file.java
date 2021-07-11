1f1e43c93a5e9b4b37f3128a47f369c0ff74f8b3
Khoa <khoa150899@gmail.com>
2021-07-11 19:55:46 -0400

    Revert "15th Commit: Altered Open File function to allow program to read multiple todo lists. End of each list is marked with a *."
    This reverts commit a0f3a2f5957cbc5a5e880be0194d3158a5440d98.

--- src/main/java/ucf/assignments/file.java
+++ src/main/java/ucf/assignments/file.java
@@ -1,14 +1,14 @@
 package ucf.assignments;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class file {
-    public static ArrayList<Map<String,Map<String, Map<String, String>>>> read(String filePath) throws IOException {
-        File file = new File(filePath);
+    public static Map<String,Map<String, Map<String, String>>> read(String filePath) throws IOException {
+        Map<String,Map<String, Map<String, String>>> todoList = new HashMap<>();
+        File file = new File(filePath); //takes in file path to read
         Scanner in = null;
         try {
             in = new Scanner(file);
@@ -16,32 +16,27 @@ public class file {
             System.out.println("Unable to read file.");
         }
 
-        ArrayList<Map<String, Map<String, Map<String, String>>>> bigList = new ArrayList<>();
+        Map<String, Map<String, String>> todo = new HashMap<>(); //puts all information into a Map that represents a single employee
+        todoList.put(in.nextLine(), todo);
 
-        while (in.hasNextLine()) {
-            Map<String,Map<String, Map<String, String>>> todoList = new HashMap<>();
-            Map<String, Map<String, String>> todo = new HashMap<>();
-            todoList.put(in.nextLine(), todo);
-            todoLoop: while (in.hasNextLine()) {
-                Scanner lineIn = new Scanner(in.nextLine()); //reads individual characters in current line
-                lineIn.useDelimiter(","); //separates different words using comma
-                while (lineIn.hasNext()) {
-                    String temp = lineIn.next();
-                    if (temp.equals("*")) {
-                        todoList.put((String) todoList.keySet().toArray()[0], todo);
-                        bigList.add(todoList);
-                        break todoLoop;
-                    }
-                    Map<String, String> todoValues = new HashMap<>();
-                    todo.put(temp, todoValues);
-                    todoValues.put("status", lineIn.next());
-                    todoValues.put("date", lineIn.next());
-                    todoValues.put("description", lineIn.next());
-                }
+
+        while (in.hasNextLine()) {//reads individual lines
+            Scanner lineIn = new Scanner(in.nextLine()); //reads individual characters in current line
+            lineIn.useDelimiter(","); //separates different words using comma
+
+
+
+            while (lineIn.hasNext()) {
+                Map<String, String> todoValues = new HashMap<>();
+                todo.put(lineIn.next(), todoValues);
+                todoValues.put("status", lineIn.next());
+                todoValues.put("date", lineIn.next());
+                todoValues.put("description", lineIn.next());
             }
+
         }
 
-        return bigList; //returns arraylist back to main
+        return todoList; //returns arraylist back to main
     }
 
     public static String create(String filePath) throws IOException {
